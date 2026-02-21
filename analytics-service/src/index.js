21841898eba6abef5329cbaa03241684f381f3fc
behzad qasim <behad.qasim@o3interfaces.com>
2026-02-21 14:15:37 +0500

    feat(order-service): initialize order service with Express and health check endpoint

---
+++ analytics-service/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ status: "API Service Running" });
+});
+
+app.listen(3003, () => {
+  console.log("API running on port 3003");
+});
